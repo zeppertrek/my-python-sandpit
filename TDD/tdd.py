dcae4ed02c386b6e7293466242c499dd00194eaf
Zepper <zeppertrek@gmail.com>
2018-11-03 14:39:12 +0530

    py files

--- TDD/tdd.py
+++ TDD/tdd.py
@@ -2,4 +2,8 @@ import os
 
 x = {1,2,3,45,5}
 
+y = [2,3,4,5,6]
+
 print (x)
+
+print (y)
---
+++ python-docs/finding_the_type.py
@@ -0,0 +1,8 @@
+#finding_the_type.py
+
+print (type(print))
+
+# Really bad programming 
+print = 77
+
+print ("Will I still continue to work")
