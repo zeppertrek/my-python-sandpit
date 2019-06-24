c1275058813f58b382613f679b7105e20756cfda
Sanjiv Mutthana Nagraj <FWIN02029@fulcrumww.com>
2019-06-24 18:16:38 +0530

    oooooops

---
+++ python-training-courses/pfc-sample-programs/oops_in_py_example_006.py
@@ -0,0 +1,33 @@
+# oops_in_py_example_006.py 
+
+class Dog:
+
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []    # creates a new empty list for each dog
+
+    #Concept of overloaded constructors  in Python ?
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed 
+        self.tricks = []    # creates a new empty list for each dog
+
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+		
+def main():
+    d = Dog('Fido')
+    e = Dog('Buddy')
+
+    f = Dog('Fido', 'Doberman')
+    g = Dog('Buddy', 'Pug')
+
+
+    d.add_trick('roll over')
+    e.add_trick('play dead')
+    print ("It is a dog's day")
+    print (f.breed)
+    print (g.breed)
+
+main()
