c1cc8773e844e5318d82c0b736f0a5b73b5af3f3
zeppertrek python <zeppertrek@gmail.com>
2019-08-19 08:44:17 +0530

    ooooops

--- python-training-courses/pfc-sample-programs/oops_in_py_example_005.py
+++ python-training-courses/pfc-sample-programs/oops_in_py_example_005.py
@@ -16,6 +16,32 @@ class Dog:
     def add_trick(self, trick):
         self.tricks.append(trick)
 		
+
+#############################################################################
+
+class Cat:
+
+    def __init__ (self):
+        print ('Initializing the cat instance')
+
+    def instcat(self):
+        print ( 'instance method [instcat] called', self)
+
+    @classmethod
+    def classycat(cls):
+        print ( 'class method [classycat]called', cls)
+
+    @staticmethod
+    def staticmethodcat():
+        print ( 'static method [staticmethodcat] called')
+
+    def __call__ (self):
+        print ("Inside the __call__ method")
+
+##############################################################################		
+
+
+		
 def main():
     d = Dog('Fido')
     e = Dog('Buddy')
@@ -24,6 +50,16 @@ def main():
     print ("It is a dog's day")
     print (d.tricks)
     print (e.tricks)
+    #
+    #
+    # What is happening over here ? 	
+    pepper = Cat()
+    pepper.instcat()
+    pepper.classycat()
+    # Note a static method is being invoked using the instance and not the class !!
+    pepper.staticmethodcat()
+    # Because the class has a __call__ method, we can use it like a function
+    pepper()
 
 if __name__== "__main__":
     main()
