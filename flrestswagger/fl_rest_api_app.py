da5be61da789edce823f880d76ccd624fd5d6f24
zeppertrek python <zeppertrek@gmail.com>
2019-10-09 15:51:16 +0530

    .gitignore

--- flrestswagger/fl_rest_api_app.py
+++ flrestswagger/fl_rest_api_app.py
@@ -9,7 +9,9 @@
 # NOTE: Run with PYTHONPATH=. python example/app.py
 
 from flask import Flask
+# A Flask extension for handling Cross Origin Resource Sharing (CORS), making cross-origin AJAX possible.
 from flask_cors import CORS
+# Extract swagger specs from your flask-restful project.
 from flask_restful_swagger_2 import Api, swagger
 
 from fl_rest_api_views import UserResource, UserItemResource, GroupResource
